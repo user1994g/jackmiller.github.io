dad2e61c2a7f72f2646823ab57e179d1944ff43e
user1994g <jazzg869@gmail.com>
2026-02-17 20:26:18 +0000

    h
    h

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -2,7 +2,6 @@ import React, { Suspense } from 'react';
 import styled from 'styled-components';
 
 const CoverVideo = React.lazy(() => import('../components/CoverVideo'));
-const Navbar = React.lazy(() => import('../components/Navbar'));
 const Logo = React.lazy(() => import('../components/Logo'));
 
 const Section = styled.section`
@@ -16,7 +15,6 @@ const Home = () => {
     <Section id="home">
       <Suspense fallback={<></>}>
         <Logo />
-        <Navbar />
         <CoverVideo />
       </Suspense>
     </Section>
